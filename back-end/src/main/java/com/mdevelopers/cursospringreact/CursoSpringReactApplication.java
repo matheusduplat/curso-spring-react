6231740af672b7fa4ec13acb692a4ffe01777717
Matheus Duplat <matheusduplat@hotmail.com>
2022-03-13 14:51:53 -0300

    Projeto criado

---
+++ back-end/src/main/java/com/mdevelopers/cursospringreact/CursoSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.mdevelopers.cursospringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringReactApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/mdevelopers/cursospringreact/CursoSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdevelopers.cursospringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
